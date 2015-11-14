c44ba8e04f511901d5c834e2eac9ade8fc68b7e9
Fred Campos <mail2frede@gmail.com>
2015-11-13 23:11:34 -0200

    implement rss and create struct for crawler

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,30 +1,45 @@
 package crawler
 
 import (
-	"golang.org/x/net/html"
 	"fmt"
+	"golang.org/x/net/html"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"time"
 )
+
+type RssItems struct {
+	index       *int
+	title       string
+	link        string
+	guid        map[string]string
+	pubdate     time.Time
+	description string
+}
 
 func Crawler() {
 	subject := "politicas"
-	engineURL := "https://www.google.com.br/search?tbm=nws"
-
-	url := engineURL + "&" + "q=" + subject
+	engineUrl := "https://news.google.com.br/news/section?output=rss"
+	url := engineUrl + "&" + "q=" + subject + "&output=rss"
 	resp, err := http.Get(url)
 
 	if err != nil {
 		fmt.Printf("%v\n", err)
 	}
 
-	links := parser(resp.Body)
-	fmt.Printf("%s\n", links)
+	body, _ := ioutil.ReadAll(resp.Body)
+	//rssData := []byte(body)
+
+	fmt.Printf("%s", string(body))
+
 }
 
 func parser(httpBody io.Reader) []string {
+
 	links := make([]string, 0)
 	page := html.NewTokenizer(httpBody)
+
 	for {
 		tokenType := page.Next()
 		if tokenType == html.ErrorToken {
